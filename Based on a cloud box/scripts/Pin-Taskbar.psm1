bdb93b299a371ebab5eb8d15000ce7828f435df6
Martijn Sips <martijnsips@live.nl>
2018-01-12 09:56:50 +0100

    Added missing packages and things to do....

---
+++ Based on a cloud box/scripts/Pin-Taskbar.psm1
@@ -0,0 +1,27 @@
+﻿function Pin-Taskbar([string]$Item = "",[string]$Action = ""){
+    if($Item -eq ""){
+        Write-Error -Message "You need to specify an item" -ErrorAction Stop
+    }
+    if($Action -eq ""){
+        Write-Error -Message "You need to specify an action: Pin or Unpin" -ErrorAction Stop
+    }
+    if((Get-Item -Path $Item -ErrorAction SilentlyContinue) -eq $null){
+        Write-Error -Message "$Item not found" -ErrorAction Stop
+    }
+    $Shell = New-Object -ComObject "Shell.Application"
+    $ItemParent = Split-Path -Path $Item -Parent
+    $ItemLeaf = Split-Path -Path $Item -Leaf
+    $Folder = $Shell.NameSpace($ItemParent)
+    $ItemObject = $Folder.ParseName($ItemLeaf)
+    $Verbs = $ItemObject.Verbs()
+    switch($Action){
+        "Pin"   {$Verb = $Verbs | Where-Object -Property Name -EQ "Pin to Tas&kbar"}
+        "Unpin" {$Verb = $Verbs | Where-Object -Property Name -EQ "Unpin from Tas&kbar"}
+        default {Write-Error -Message "Invalid action, should be Pin or Unpin" -ErrorAction Stop}
+    }
+    if($Verb -eq $null){
+        Write-Error -Message "That action is not currently available on this item" -ErrorAction Stop
+    } else {
+        $Result = $Verb.DoIt()
+    }
+}
