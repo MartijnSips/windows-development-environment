9b53cc01b269d80b469973033a23e721b5e4f5cb
Martijn Sips <martijnsips@live.nl>
2018-02-26 13:27:00 +0100

    Disabled not working code

--- scripts/SetupDevelopmentSoftware.ps1
+++ scripts/SetupDevelopmentSoftware.ps1
@@ -10,7 +10,8 @@ Set-RvShortcutToRunAsAdministrator -Path 'C:\Users\vagrant\AppData\Roaming\Micro
 echo "- IIS ..."
 Invoke-Expression "c:\users\vagrant\scripts\taskbarpin.vbs 'c:\windows\system32\inetsrv\InetMgr.exe'"
 
-echo "- Notepad++ ..."
+# Currently there is a problem with the deployment of Notepad++ via chocolatey
+#echo "- Notepad++ ..."
 #choco install notepadplusplus -y
 #Invoke-Expression "c:\users\vagrant\scripts\taskbarpin.vbs 'C:\Program Files\Notepad++\notepad++.exe'"
 
@@ -74,8 +75,8 @@ choco install postman -y
 choco install intellijidea-community -y
 
 # Fiddler
-Invoke-WebRequest -Uri https://telerik-fiddler.s3.amazonaws.com/fiddler/FiddlerSetup.exe -OutFile "c:\\users\\vagrant\\Download\\FiddlerSetup.exe"
-Invoke-Expression "c:\\users\\vagrant\\Download\\FiddlerSetup.exe /S"
+Invoke-WebRequest -Uri https://telerik-fiddler.s3.amazonaws.com/fiddler/FiddlerSetup.exe -OutFile "c:\\users\\vagrant\\Downloads\\FiddlerSetup.exe"
+Invoke-Expression "c:\\users\\vagrant\\Downloads\\FiddlerSetup.exe /S"
 Invoke-Expression "c:\users\vagrant\scripts\taskbarpin.vbs 'C:\Users\vagrant\AppData\Local\Programs\Fiddler\Fiddler.exe'"
 
 # Visual Studio 2017 and its plugins
@@ -84,13 +85,28 @@ choco install visualstudio2017enterprise -y
 choco install visualstudio2017-workload-netweb -y
 choco install visualstudio2017-workload-manageddesktop -y
 choco install visualstudio2017-workload-azure -y
-Invoke-WebRequest -Uri https://marketplace.visualstudio.com/items?itemName=TechTalkSpecFlowTeam.SpecFlowforVisualStudio2017 -OutFile "c:\\users\\vagrant\\Download\\TechTalkSpecFlowTeam.SpecFlowforVisualStudio2017.vsix"
-Invoke-Expression "& 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe' /q .\Download\\TechTalkSpecFlowTeam.SpecFlowforVisualStudio2017.vsix"
-#choco install wixtoolset -y
-choco install specflow -y
+choco install visualstudio2017-workload-universal -y
 Invoke-Expression "c:\users\vagrant\scripts\taskbarpin.vbs 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe'"
 Set-RvShortcutToRunAsAdministrator -Path 'C:\Users\vagrant\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Visual Studio 2017.lnk' -Verbose
 
+# Specflow
+#Invoke-WebRequest -Uri https://marketplace.visualstudio.com/items?itemName=TechTalkSpecFlowTeam.SpecFlowforVisualStudio2017 -OutFile "c:\\users\\vagrant\\Downloads\\TechTalkSpecFlowTeam.SpecFlowforVisualStudio2017.vsix"
+#Invoke-Expression "& 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe' /q .\Downloads\\TechTalkSpecFlowTeam.SpecFlowforVisualStudio2017.vsix"
+# Wix
+#choco install wixtoolset -y
+# Productivity Power Tools
+#Invoke-WebRequest -Uri https://marketplace.visualstudio.com/items?itemName=VisualStudioProductTeam.ProductivityPowerTools -OutFile "c:\\users\\vagrant\\Downloads\\ProPowerTools.vsix"
+#Invoke-Expression "& 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe' /q .\Downloads\\ProPowerTools.vsix"
+# Powershell Tools
+# Wix Toolset Visual Studio 2017 Extension
+# Web Essentials
+# EF Core Power Tools
+# Resharper
+
 choco install azure-cli -y
+
+# Visual studio code.
 choco install visualstudiocode -y
-#Invoke-Expression 'npm i -g azure-functions-core-tools'
+
+choco install nodejs -y
+#Invoke-Expression "'c:\\Program Files\nodejs\npm' -i -g azure-functions-core-tools"
